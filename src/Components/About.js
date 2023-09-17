8fdfc4aed04d756578b266ebc6d5cbe9f54ea4ca
Venktesh Vishwanathan <venktesh.vishwanathan@cobrainer.com>
2023-09-17 20:32:44 +0200

    added important dates

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,7 +8,7 @@ class About extends Component {
          var profilepic = "images/" + this.props.data.image;
          var bio = this.props.data.bio;
          var about = this.props.data.about;
-         console.log("about",about)
+         console.log("about", about)
          var street = this.props.data.address.street;
          var city = this.props.data.address.city;
          var state = this.props.data.address.state;
@@ -29,41 +29,50 @@ class About extends Component {
 
       return (
          <div>
-         <section id="about">
-            <div className="row">
-               <div className="three columns">
-                  <img className="profile-pic" src={profilepic} alt="Delft profile Pic" />
-               </div>
-               <div className="nine columns main-col">
-                  <h2>About</h2>
-                  <p>{about}</p>
-                  <a href="https://www.informatiewetenschap.org/dir-history.html">Prior Editions</a>
-
-
-
-
-               </div>
-               </div>
-
-               </section>
-
-               <section id="call">
+            <section id="about">
                <div className="row">
-               <div className="three columns">
+                  <div className="three columns">
+                     <img className="profile-pic" src={profilepic} alt="Delft profile Pic" />
+                  </div>
+                  <div className="nine columns main-col">
+                     <h2>About</h2>
+                     <p>{about}</p>
+                     <a href="https://www.informatiewetenschap.org/dir-history.html">Prior Editions</a>
+
+
+
+
+                  </div>
                </div>
-               <div className="nine columns main-col">
 
-               <h2>Present Your Work</h2>
-                  <p>{bio}</p>
+            </section>
 
-                  <p className="cat"><ol>{categories}</ol></p>
-                  <p>Please submit your contributions <a href="https://tudelft.fra1.qualtrics.com/jfe/form/SV_bpVNFS6uxPIijki">here</a></p>
-                  <p>{contactMessage}</p>
-                     </div>
-            </div>
-               </section>
+            <section id="call">
+               <div className="row">
+                  <div className="three columns">
+                  </div>
+                  <div className="nine columns main-col">
 
-            </div>
+                     <h2>Present Your Work</h2>
+                     <p>{bio}</p>
+
+                     <p className="cat"><ol>{categories}</ol></p>
+                     <p>Please submit your contributions <a href="https://tudelft.fra1.qualtrics.com/jfe/form/SV_bpVNFS6uxPIijki">here</a></p>
+                     <p>{contactMessage}</p>
+                     <h2>Important dates</h2>
+                     <p>
+
+                        Nominate your contribution by: <strong style={{ color: 'white', fontweight:700 }}>October 14, 2023 </strong><br />
+
+                        Notification of contributions to spotlight during DIR 2023: <b style={{ color: 'white', fontweight:700 }}>October 30, 2023</b>  <br />
+
+                     DIR 2023: <b style={{ color: 'white', fontweight:700 }}>November 27, 2023</b>
+                  </p>
+               </div>
+         </div>
+               </section >
+
+            </div >
 
 
       );
